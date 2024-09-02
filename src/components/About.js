b5f0a291a9c8f77a924869e52a958b8e28f2c631
sameem2024 <guunnsameem@gmail.com>
2024-09-02 12:51:50 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This blog app is a React Project 
+                which uses React router Dom and <span style={{whiteSpace:'nowrap'}}>date-fns</span> Libraries...</p>
+        </main>
+    )
+}
+
+export default About
